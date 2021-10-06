635cd15a9193e8bc787d234b40bd0f906afa5094
yuno-x <miva.xyz@gmail.com>
2021-10-06 23:44:44 +0900

    Bash script what make virtual node.

--- mkcontainer.sh
+++ mkcontainer.sh
@@ -7,35 +7,37 @@ then
 fi
 
 INAME=$1
-CNAME=$2
 
-if [ "`ip netns | grep -w ${CNAME}`" != "" ]
-then
-  echo "Network netspace \"${CNAME}\" already exists." >&2
-  exit -1
-fi
-
-sudo docker run -d --privileged --network none --hostname ${CNAME} --name ${CNAME} -v /tmp/.X11-unix/:/tmp/.X11-unix -v /sys/fs/cgroup:/sys/fs/cgroup:ro ${INAME} /usr/bin/systemd
-#sudo docker exec ${CNAME} bash -c "echo export DISPLAY=$DISPLAY >> ~/.bashrc"
-
-ID=`sudo docker ps -f "name=${CNAME}" --format '{{.ID}}'`
-
-SYSTEMD_PID=$(
-for PP in `pgrep -f ${ID}`
+for CNAME in ${@:2}
 do
-  pgrep -P ${PP} -n systemd
+  if [ "`ip netns | grep -w ${CNAME}`" != "" ]
+  then
+    echo "Network netspace \"${CNAME}\" already exists." >&2
+    exit -1
+  fi
+
+  sudo docker run -d --privileged --network none --hostname ${CNAME} --name ${CNAME} -v /tmp/.X11-unix/:/tmp/.X11-unix -v /sys/fs/cgroup:/sys/fs/cgroup:ro ${INAME} /usr/bin/systemd
+  #sudo docker exec ${CNAME} bash -c "echo export DISPLAY=$DISPLAY >> ~/.bashrc"
+
+  ID=`sudo docker ps -f "name=${CNAME}" --format '{{.ID}}'`
+
+  SYSTEMD_PID=$(
+  for PP in `pgrep -f ${ID}`
+  do
+    pgrep -P ${PP} -n systemd
+  done
+  )
+
+  if [ -f /var/run/netns ]
+  then
+    echo "NetNS Creation Error"
+    exit -1
+  fi
+
+  if [ ! -d /var/run/netns ]
+  then
+    sudo mkdir /var/run/netns
+  fi
+
+  sudo ln -s /proc/${SYSTEMD_PID}/ns/net /var/run/netns/${CNAME}
 done
-)
-
-if [ -f /var/run/netns ]
-then
-  echo "NetNS Creation Error"
-  exit -1
-fi
-
-if [ ! -d /var/run/netns ]
-then
-  sudo mkdir /var/run/netns
-fi
-
-sudo ln -s /proc/${SYSTEMD_PID}/ns/net /var/run/netns/${CNAME}
