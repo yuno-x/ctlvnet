be4528e61f9746f1043a9f8c3e60042fc1394e4b
yuno-x <miva.xyz@gmail.com>
2022-01-16 06:42:35 +0900

    Bash script what install virtual node.

--- mkimage.sh
+++ mkimage.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-if [ "$( whoami )" == "root" ]
-then
-  SUDO=""
-else
-  if ! sudo echo -n
-  then
-    echo "You must have permission to use sudo command." >&2
-    exit -1
-  fi
+PWD="$(dirname $0)"
+source $PWD/modules/check.sh
 
-  SUDO=sudo
-fi
+ctlv_set_SUDO
 
 if [ "$( which docker )" == "" ]
 then
-  echo -n "Do you want to install docker? [y/n]: "
-  read ANS
+  read -p "Do you want to install docker? [y/N]: " ANS
   if [ "$ANS" == "y" ]
   then
+    ctlv_check_systemctl
+
     $SUDO apt -y install apt-transport-https ca-certificates curl software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | $SUDO apt-key add -
     $SUDO add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
@@ -43,12 +36,37 @@ then
 fi
 IMAGENAME=$1
 
-if ! systemctl --no-pager > /dev/null 2>&1
-then
-  echo "Systemd seems not to be runnnig."
+REQUIRED_PACKAGE="bash-completion apt-utils tmux vim htop iproute2 iputils-ping traceroute curl nmap telnet tcpdump iptables apt-file w3m git python3 systemd avahi-daemon avahi-utils bind9 bind9utils man openssh-server openssh-client telnetd frr apache2 dsniff isc-dhcp-server"
+SUGGESTED_PACKAGE="wireshark hexedit nkf x11-apps build-essential openjdk-16-jdk php mysql-server mysql-client"
+cat <<EOF
+Following packages are required (must be installed to a designated image):
+  $REQUIRED_PACKAGE
 
-  exit -1
+Following packages are suggested (can be installed to a designated image):
+  $SUGGESTED_PACKAGE
+
+EOF
+
+INSTALL_PACKAGE="$REQUIRED_PACKAGE"
+read -p "Do you want to install each suggested package to a desinated image? [Y/n/i] ('i': interact eachly): " ANS
+if [ "$ANS" == "n" ]
+then
+  :
+elif [ "$ANS" == "i" ]
+then
+  for PACKAGE in $SUGGESTED_PACKAGE
+  do
+    read -p "Do you want to install '$PACKAGE' to desinated image? [Y/n]: " ANS
+    if [ "$ANS" != "n" ]
+    then
+      INSTALL_PACKAGE="$INSTALL_PACKAGE $PACKAGE"
+    fi
+  done
+else
+  INSTALL_PACKAGE="$INSTALL_PACKAGE $SUGGESTED_PACKAGE"
 fi
+
+ctlv_check_systemctl
 
 $SUDO systemctl restart docker
 EXISTIMAGE=$($SUDO docker images $IMAGENAME | grep -v "REPOSITORY")
@@ -65,6 +83,7 @@ if [ "$EXISTIMAGE" == "" ]
 then
   $SUDO docker pull ubuntu
 fi
+
 $SUDO docker run --hostname ubuntu --name ubuntu --detach ubuntu bash -c "while [ 1 ]; do sleep 600; done"
 $SUDO docker commit ubuntu $IMAGENAME
 $SUDO docker rm -f `$SUDO docker ps -qf "name=ubuntu"`
@@ -81,13 +100,16 @@ EOF'
 
 $SUDO docker exec -it $CNAME apt update
 $SUDO docker exec -it $CNAME bash -c 'yes | unminimize'
-$SUDO docker exec -it $CNAME bash -c 'yes no | apt -y install wireshark'
-$SUDO docker exec -it $CNAME apt -y install bash-completion apt-utils tmux vim htop hexedit iproute2 iputils-ping traceroute curl nmap telnet tcpdump iptables apt-file w3m nkf x11-apps git build-essential python3 openjdk-16-jdk systemd avahi-daemon avahi-utils bind9 bind9utils man openssh-server openssh-client telnetd frr apache2 php dsniff isc-dhcp-server mysql-server mysql-client
+if echo $INSTALL_PACKAGE | grep -w "wireshark" > /dev/null
+then
+  INSTALL_PACKAGE="$(echo $INSTALL_PACKAGE | sed 's/ wireshark[^ ]*//g')"
+  $SUDO docker exec -it $CNAME bash -c 'yes no | apt -y install wireshark'
+fi
+$SUDO docker exec -it $CNAME apt -y install $INSTALL_PACKAGE
 $SUDO docker exec -it $CNAME apt-file update
 $SUDO docker exec $CNAME bash -c 'for FILE in `ls /usr/share/doc/frr/examples/*.sample`; do touch /etc/frr/$(basename -s .sample $FILE); done'
 
 $SUDO docker exec $CNAME sed -i "s/\(^[^#]*d\)=no/\1=yes/g" /etc/frr/daemons
-#$SUDO docker exec $CNAME sed -i "s/^\(ospfd=yes\)/\1\nospfd_instances=$(seq -s , 1 8)/g" /etc/frr/daemons
 $SUDO docker exec $CNAME systemctl disable systemd-timesyncd
 $SUDO docker exec $CNAME sed -i "s/#enable-reflector=no/#enable-reflector=no\nenable-reflector=yes/g" /etc/avahi/avahi-daemon.conf
 
