e94f3275d979599560c94830c5e6805dbaa3cb95
yuno-x <miva.xyz@gmail.com>
2022-01-22 04:16:34 +0900

    Bash script what create virtual node.

--- mkcontainer.sh
+++ mkcontainer.sh
@@ -6,8 +6,8 @@ source modules/letdef.sh
 function printhelp()
 {
   echo "usage: $0 [IMAGE NAME] [CONTAINER NAME]..." >&2
-  echo >&2
-  echo "If you add a character '@' at last of container name, its container is also connected to default docker network." >&2
+#  echo >&2
+#  echo "If you add a character '@' at last of container name, its container is also connected to default docker network." >&2
 }
 
 if [ "$1" == "" ]
@@ -24,13 +24,31 @@ INAME=$1
 
 for CNAME in ${@:2}
 do
+  INITOPT="/usr/bin/systemd"
+  NETOPT="--network none"
+
+  FLAGS="$(echo $CNAME | cut -d : -f 2 | sed 's/\(.\)/\1 /g')"
+  CNAME=$(echo $CNAME | cut -d : -f 1)
   echo "Creating $CNAME..."
-  NETFLAG=false
+
+  for FLAG in $FLAGS
+  do
+    case "$FLAG" in
+      "i") INITOPT="/sbin/init" ;;
+      "n") NETOPT="" ;;
+      *) echo "Cannot recognize option." >&2 ;;
+    esac
+  done
+
+#
+echo <<COMMENT > /dev/null
   if echo $CNAME | grep "@$" > /dev/null
   then
     NETFLAG=true
     CNAME=$( echo $CNAME | sed s/@$//g )
   fi
+COMMENT
+#
 
   if [ "`ip netns | grep -w ${CNAME}`" != "" ]
   then
@@ -38,12 +56,13 @@ do
     exit -1
   fi
 
-  if $NETFLAG
-  then
-    $SUDO docker run -d --privileged --hostname ${CNAME} --name ${CNAME} -e DISPLAY=${DISPLAY} -v /tmp/.X11-unix/:/tmp/.X11-unix -v /sys/fs/cgroup:/sys/fs/cgroup:ro ${INAME} /usr/bin/systemd
-  else
-    $SUDO docker run -d --privileged --network none --hostname ${CNAME} --name ${CNAME} -e DISPLAY=${DISPLAY} -v /tmp/.X11-unix/:/tmp/.X11-unix -v /sys/fs/cgroup:/sys/fs/cgroup:ro ${INAME} /usr/bin/systemd
-  fi
+#  if $NETFLAG
+#  then
+  $SUDO docker run -d --privileged $NETOPT --hostname ${CNAME} --name ${CNAME} -e DISPLAY=${DISPLAY} -v /tmp/.X11-unix/:/tmp/.X11-unix -v /sys/fs/cgroup:/sys/fs/cgroup:ro ${INAME} $INITOPT
+#    $SUDO docker run -d --privileged --hostname ${CNAME} --name ${CNAME} -e DISPLAY=${DISPLAY} -v /tmp/.X11-unix/:/tmp/.X11-unix -v /sys/fs/cgroup:/sys/fs/cgroup:ro ${INAME} /sbin/init # /usr/bin/systemd
+#  else
+#    $SUDO docker run -d --privileged --network none --hostname ${CNAME} --name ${CNAME} -e DISPLAY=${DISPLAY} -v /tmp/.X11-unix/:/tmp/.X11-unix -v /sys/fs/cgroup:/sys/fs/cgroup:ro ${INAME} /usr/bin/systemd
+#  fi
 
   SUDO="$SUDO" $SUDO docker exec ${CNAME} bash -c "$CTLV_SYSNETSET"
 
