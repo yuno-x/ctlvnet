a7c5608793e54906e359e2360e0f97a90556e039
yuno-x <miva.xyz@gmail.com>
2022-05-27 21:23:43 +0900

    Various and Const data for ctlvnet

--- modules/letdef.sh
+++ modules/letdef.sh
@@ -9,6 +9,8 @@ $SUDO sysctl -w net.ipv4.udp_l3mdev_accept=1 > /dev/null
 $SUDO sysctl -w net.ipv4.conf.default.rp_filter=0 > /dev/null
 $SUDO sysctl -w net.ipv4.conf.all.rp_filter=0 > /dev/null
 $SUDO sysctl -w net.bridge.bridge-nf-call-iptables=0 > /dev/null
+$SUDO sysctl -w net.ipv6.conf.all.disable_ipv6=0 > /dev/null
+$SUDO sysctl -w net.ipv6.conf.default.disable_ipv6=0 > /dev/null
 '
 
 CTLV_SYSNETGET='
@@ -19,4 +21,6 @@ $SUDO sysctl net.ipv4.udp_l3mdev_accept
 $SUDO sysctl net.ipv4.conf.default.rp_filter
 $SUDO sysctl net.ipv4.conf.all.rp_filter
 $SUDO sysctl net.bridge.bridge-nf-call-iptables
+$SUDO sysctl net.ipv6.conf.all.disable_ipv6
+$SUDO sysctl net.ipv6.conf.default.disable_ipv6
 '
