7d563961c1da66864604a935d9db14af305b14c1
yuno-x <miva.xyz@gmail.com>
2022-05-27 21:22:40 +0900

    Bash script what remove virtual node.

--- rmcontainer.sh
+++ rmcontainer.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
-
-PWD="$(dirname $0)"
+cd "$(dirname $0)"
 source $PWD/modules/check.sh
 
-if [ "$1" == "" ]
-then
-  echo "usage: $0 [CONTAINER NAME]..."
-  exit -1
-fi
+function ctlv_mod_rmcontainer()
+{
 
-ctlv_set_SUDO
-
-for CNAME in $@
-do
-  $SUDO docker exec ${CNAME} systemctl halt > /dev/null 2>&1
-  $SUDO docker rm -f ${CNAME}
-  if [ -h /var/run/netns/${CNAME} ]
+  if [ "$1" == "" ]
   then
-    $SUDO rm /var/run/netns/${CNAME}
+    echo "usage: $0 [CONTAINER NAME]..."
+    exit -1
   fi
-done
+
+  ctlv_set_SUDO
+
+  for CNAME in $@
+  do
+    $SUDO docker exec ${CNAME} systemctl halt > /dev/null 2>&1
+    $SUDO docker rm -f ${CNAME}
+    if [ -h /var/run/netns/${CNAME} ]
+    then
+      $SUDO rm /var/run/netns/${CNAME}
+    fi
+  done
+}
+
+if [ -z "$SUBCMD" ]
+then
+  CMD=$0
+  ctlv_mod_rmcontainer $@
+fi
